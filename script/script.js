61c19a0a4f8e3fb25f145846efa8af9ec96504e8
KukhalashviliLA <kukhalashvili.la@gazprom-neft.ru>
2019-06-04 20:12:58 +0300

    Revert "lesson 2"
    This reverts commit 03fbb495a093cd95f3bb1eecec9005cd2aa52062.

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 class ProductsList {
-  constructor(container = ".goods-list") {
+  constructor(container = '.goods-list') {
     this.data = [];
     this.container = container;
     this.productsAll = [];
@@ -11,11 +11,11 @@ class ProductsList {
   }
   _fetchProducts() {
     this.data = [
-      { id: 1, title: "Notebook", price: 2000 },
-      { id: 2, title: "Mouse", price: 30 },
-      { id: 3, title: "Keyboard", price: 55 },
-      { id: 4, title: "Gamepad", price: 65 },
-      { id: 5, title: "Chair", price: 120, template: 1 }
+      { id: 1, title: 'Notebook', price: 2000 },
+      { id: 2, title: 'Mouse', price: 30 },
+      { id: 3, title: 'Keyboard', price: 55 },
+      { id: 4, title: 'Gamepad', price: 65 },
+      { id: 5, title: 'Chair', price: 120, template: 1 },
     ];
   }
   _render() {
@@ -23,8 +23,15 @@ class ProductsList {
     for (let product of this.data) {
       const prod = new ProductItem(product);
       this.productsAll.push(prod);
-      block.insertAdjacentHTML("beforeend", prod.render());
+      block.insertAdjacentHTML('beforeend', prod.render());
     }
+  }
+  // Добавьте для каталога метод, определяющий суммарную стоимость всех товаров.
+  calcAllProducts() {
+    let costAllProducts = 0;
+    this.data.forEach((product) => { 
+      costAllProducts += product.price;
+    });
   }
 }
 
@@ -33,19 +40,57 @@ class ProductItem {
     this.title = product.title;
     this.price = product.price;
     this.id = product.id;
-    this.img = img;
+    this.img = img
   }
 
   render() {
     return `<div class="product-item">
-            <img class="product_img" src="${this.img}" alt="${this.title}">
-                  <div class="desc">
-                      <h3>${this.title}</h3>
-                      <p>${this.price}</p>
-                      <button class="cart-button">Купить</button>
-                  </div>
-              </div>`;
+                <img src="${this.img}" alt="${this.title}">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price}</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`
   }
 }
 
 const products = new ProductsList();
+
+// Добавьте пустые классы для корзины товаров и элемента корзины товаров. 
+// Продумайте, какие методы понадобятся для работы с этими сущностями.
+
+// Класс корзины товаров
+class CartList {
+  constructor() {
+    // добавленные товары в корзине
+    // this.title = product.title;
+    // this.price = product.price;
+    // this.id = product.id;
+    // this.img = img
+  }
+  render() {
+    // что то рендерим
+  }
+}
+// Класс элементов корзины товаров
+class CartItems {
+  constructor() {
+    this.cartProducts = [];
+  }
+  addCartProduct() {
+    //добавляем товар в корзину.
+  }
+  removeCartProduct() {
+    //удаляем товар из корзины.
+  }
+  calcPriceCartProduct() {
+    // считаем стоимость всех товаров в корзине.
+  }
+  calcItemsCartProduct() {
+    //считаем количиство товаров в корзине.
+  }
+  render() {
+    // что то рендерим
+  }
+}
